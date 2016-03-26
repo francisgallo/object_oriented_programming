39885748380b859c8179bccbd1646e1f13752fbf
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-26 13:53:55 -0400

    working on move function :

--- nrover.rb
+++ nrover.rb
@@ -1,35 +1,51 @@
 class Rover
-  #attr:
+attr_accessor :x_cor , :y_cor , :direction
+
+  def initialize (x_cor , y_cor , direction)
+
+      @x_cor = x_cor
+      @y_cor = y_cor
+      @direction = direction
+
+  end
+
 
   def direction
-    puts "what is your starting direction"
-    @direction=gets.chomp
-    puts @direction
+   puts "what is your starting direction"
+   @direction=gets.chomp
+   puts @direction
   end
 
-  def l_facing
-    if  @direction =="N" then "W"
-    elsif @direction =="W" then "S"
-    elsif @direction == "S"then "E"
-    elsif @direction == "E" then "N"
+  def move
+    if @direction == "N" then   @y_cor += 1
+    elsif @direction == "S" then  @y_cor -=1
+    elsif @direction == "W"  then @x_cor -= 1
+    elsif @direction == "E" then @x_cor += 1
     else
       puts "nothing"
     end
   end
 
-  def r_facing
-    if  @direction =="N" then "E"
-    elsif @direction =="E" then "S"
-    elsif @direction == "S"then "W"
-    elsif @direction == "W" then "N"
-    else
-      puts "nothing"
+    def l_facing
+      if  @direction =="N" then "W"
+      elsif @direction =="W" then "S"
+      elsif @direction == "S"then "E"
+      elsif @direction == "E" then "N"
+      else
     end
   end
 
-
+    def r_facing
+      if  @direction =="N" then "E"
+      elsif @direction =="E" then "S"
+      elsif @direction == "S"then "W"
+      elsif @direction == "W" then "N"
+      else
+      puts "nothing"
+    end
+  end
 
 end
 
-rover= Rover.new
-rover.direction
+#rover= Rover.new(1, 1,"N")
+#rover.direction

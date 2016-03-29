ad749d30599d9b7e66378bf500c5bfa20d730a81
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-29 07:20:58 -0400

    fixed the orginal rover code

--- nrover.rb
+++ nrover.rb
@@ -1,94 +1,117 @@
 class Rover
 attr_accessor :x_cor , :y_cor , :direction
+attr_reader :Plateau
 
-  def initialize (x_cor , y_cor , direction)
-      @x_cor = x_cor
-      @y_cor = y_cor
-      @direction = direction
-  end
+class Rover
+attr_accessor :x_cor , :y_cor , :direction
 
+def initialize (x_cor , y_cor , direction)
+    @x_cor = x_cor
+    @y_cor = y_cor
+    @direction = direction
+end
 
-  def move
+def read_instructions(instruction)
 
-    if @direction == "N"
-      @y_cor += 1
-    elsif @direction == "S"
-       @y_cor -=1
-    elsif @direction == "W"
-      @x_cor -= 1
-    elsif @direction == "E"
-      @x_cor += 1
-    else
-    end
-  end
+    instruction.each  do |instruction|
 
-    def l_facing
-      if  @direction =="N"
-        @direction = "W"
-      elsif @direction =="W"
-        @direction = "S"
-      elsif @direction == "S"
-        @direction ="E"
-      elsif @direction == "E"
-        @direction = "N"
-      else
-    end
-  end
+        if instruction  == "M"
+        self.move
 
-    def r_facing
-      if  @direction =="N"
-         @direction = "E"
-      elsif @direction =="E"
-        @direction ="S"
-      elsif @direction == "S"
-        @direction = "W"
-      elsif @direction == "W"
-         @direction = "N"
-      else
-      puts "nothing"
-    end
-  end
-
-  def read_instruction(instruction)
-    instruction.map do |v|
-
-      if v  == "M"
-      self.move
-    elsif v == "L"
+      elsif instruction == "L"
         self.l_facing
-    elsif v =="R"
+
+      elsif instruction  =="R"
         self.r_facing
       else
         puts "Wrong input "
+        end
       end
     end
-  end
 
-  def output
-     puts " You are currently  at #{@x_cor}, #{@y_cor} facing  #{@direction}"
-   end
+
+def move
+  if @direction == "N"
+    @y_cor += 1
+  elsif @direction == "S"
+     @y_cor -=1
+  elsif @direction == "W"
+    @x_cor -= 1
+  elsif @direction == "E"
+    @x_cor += 1
+  else
+  end
 end
 
+  def l_facing
+    if  @direction =="N"
+      @direction = "W"
+    elsif @direction =="W"
+      @direction = "S"
+    elsif @direction == "S"
+      @direction ="E"
+    elsif @direction == "E"
+      @direction = "N"
+    else
+  end
+end
+
+  def r_facing
+    if  @direction =="N"
+       @direction = "E"
+    elsif @direction =="E"
+      @direction ="S"
+    elsif @direction == "S"
+      @direction = "W"
+    elsif @direction == "W"
+       @direction = "N"
+    else
+    puts "nothing"
+  end
+end
+
+
+
+
+def output
+   puts " You are currently  at #{@x_cor}, #{@y_cor} facing  #{@direction}"
+ end
+end
+
+
+
 class Plateau
-  attr_accessor :height , :widith
+  attr_accessor :height , :widith, :rovers
 
   def initialize (height , widith, rovers)
     @height=height
     @widith =widith
+    @rovers = ()
   end
 
+  #def move_rovers
+  #  end
+  #end
+
   def in_bounds (x,y)
-    x >= 0 and x <= widith
-    y >=0 and y <=height
-  end
+    x=@x_cor
+    y=@y_cor
+     if x  >= 0 and x <=@widith || y >=0 and y <=@height
+    then  self.move
+     else
+       puts "your rover is off the map"
+     end
+   end
+
+
 end
 # When Plateau x value hits >=10 or <=-1  and y value hits >=10 or <=-1
 
 
-
+   area= Plateau.new(10,7,1)
    rover= Rover.new(1, 5,"N")
    puts "How would you like to move your Rover? L turn left, R to turn Right and M to move"
-   instructions = gets.chomp.upcase
-   array_instructions = instructions.split(//)
-   rover.read_instruction(array_instructions)
+  instruction = gets.chomp.upcase.split
+
+   rover.read_instructions(instruction)
    rover.output
